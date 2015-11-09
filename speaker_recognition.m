4d0b8fd687cbe8ad1096fe6dd6b39ec2af7fe513
tditada <tditada@alu.itba.edu.ar>
2015-11-09 13:52:12 -0300

    Audio reading and course algorithms added

---
+++ speaker_recognition.m
@@ -0,0 +1,30 @@
+function speaker_recognition
+	audio_files = readdir(strcat(pwd,'/resources/audio'));
+	audio_cant = size(audio_files); 
+	audio_speakers = audio_cant/3; % 3 audios per speaker
+
+	disp('training');
+	for i=3:audio_cant(1) % . and .. not included
+		if mod(i,3)==0 % first audios for learning
+			audio = strcat('resources/audio/',char(audio_files(i)));
+			disp(audio);
+			[y, s] = wavread(audio);
+			%TODO: coeficientes mel-cepstrales estáticos y dinámicos
+			%TODO: algoritmo de quantización vectorial, como vq(data, nvector)
+		end
+	end
+
+
+	disp('testing');
+	for i=3:audio_cant(1) % . and .. not included
+		if mod(i,3)~=0 % first audios for learning
+			audio = strcat('resources/audio/',char(audio_files(i)));
+			disp(audio);
+			[y, s] = wavread(audio);
+			%TODO: coeficientes mel-cepstrales estáticos y dinámicos
+			%TODO: Buscar vectores que minimizen la distorción media, como con meandist(data,code)
+		end
+	end
+
+	%TODO: efectividad del algoritmo
+end
