7176018014ff073e673972c0ef0abaeadc0243fa
Mechivarela <m.mercedes.varela@gmail.com>
2015-11-26 04:46:10 -0300

    efectividad funcionando

--- speaker_recognition.m
+++ speaker_recognition.m
@@ -2,9 +2,9 @@ function speaker_recognition
 	audio_files = readdir(strcat(pwd,'/resources/audio'));
 	audio_cant = size(audio_files); 
 	audio_speakers = floor(audio_cant(1)/4); % 4 audios per speaker
-	names = ['Eliana'; 'Esteban'; 'Juan Ignacio'; 'Juan Pablo'; 'Lautaro'; 'Maria';'Matias'; 'Mercedes'; 'Nicolas';'Pablo';'Paula'; 'Ricardo';'Sofia'];
+	names = ['Eliana'; 'Esteban'; 'Juan_Ignacio'; 'Juan_Pablo'; 'Lautaro'; 'Maria';'Matias'; 'Mercedes'; 'Nicolas';'Pablo';'Paula'; 'Ricardo';'Sofia'];
 	disp(['Total Speakers ',  num2str(audio_speakers)]);
-	disp('training');
+	disp('TRAINING');
 	fflush(stdout);
 	for i=3:audio_cant(1) % . and .. not included
 		if mod(i,4)==0 % first audios for training
@@ -18,7 +18,7 @@ function speaker_recognition
 	end
 	fflush(stdout);
 
-	disp('testing');
+	disp('TESTING');
 	rights=0;
 	for i=3:audio_cant(1) % . and .. not included
 		if mod(i,4)~=0 % all audios except training ones
@@ -33,16 +33,16 @@ function speaker_recognition
 				aux(k) = meandist(coef,coefvq(:,:,k));
 			end
 			[min_value,index] = min(aux);
-			disp('min')
-			disp(aux)
+			%disp('min')
+			%disp(aux)
 			fflush(stdout);
 			disp(['Recognition: ',names(index,:)]);
-			cmp = strcmp(name, names(index));
+			cmp = strcmp(name, strtok(names(index, :),' '));
 			if cmp==1
 				rights+=1;
 			end
 		end
 	end
-	efectiv = rights / audio_speakers;
+	efectiv = rights / (audio_cant-4); %le resto los de training
 	disp(['Efectividad (entre 0 y 1):', num2str(efectiv)]);
 end
